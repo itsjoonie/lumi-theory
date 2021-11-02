3fbae77d2ade1ddebb85ec56d999812608a95318
june <luo.jnny@gmail.com>
2021-11-02 00:25:01 -0700

    where is my font awesome?

--- react-app/src/components/Home/LandingPage.js
+++ react-app/src/components/Home/LandingPage.js
@@ -44,7 +44,7 @@ function LandingPage(){
             <div className="landing-banner-three">
                 <div className="banner-3-container">
                     <div>
-                    <img className="banner-three-img" src="https://raw.githubusercontent.com/itsjoonie/lumi-theory/main/react-app/src/components/Home/image/banner2.JPG?token=AQLSLSCJQVSLXPRS77R4QIDBRIOQI" alt="pic"/>
+                    <img className="banner-three-img" src="https://raw.githubusercontent.com/itsjoonie/lumi-theory/main/react-app/src/components/Home/image/banner3.jpg?token=AQLSLSHEYZ6CTOCI6CVOBMDBPH64K" alt="pic"/>
                     </div>
                     <div className="aboutUs">
                         <h1>About Us</h1>
