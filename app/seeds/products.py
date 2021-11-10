452cb0659084638fc02a6b2219e3770ea21a7900
june <luo.jnny@gmail.com>
2021-11-09 21:01:21 -0800

    products seed in progress

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -1,11 +1,172 @@
-# from app.models import db, Product
+from app.models import db, Product
 
-# def seed_products():
-#     night_owl = Product(
-#         name = 'Night Owl',
-#         top_scent = 'Lemon, Lime, berry',
-#         base_scent = 'Vanilla, Sandalwood',
-#         lid_color = 'Silver',
-#         description = "Have you ever crave cereal in the middle of the night, but don't want the calories? This candle is perfect for you! It smells like a freshly poured big bowl of Fruit Loop. A perfect balance of citrus and berries, it is a nostalgia in a jar for sure. ",
+def seed_products():
+    night_owl = Product(
+        name = 'Night Owl',
+        top_scent = 'Lemon, Lime, Berry',
+        base_scent = 'Vanilla, Sandalwood',
+        lid_color = 'Silver',
+        description = 'Have you ever craved cereal in the middle of the night, but don\'t want the calories? This candle is perfect for you! It smells like a freshly poured big bowl of Fruit Loop. A perfect balance of citrus and berries, it is a nostalgia in a jar for sure. ',
+        price = 20,
+        quantity = 5,
+        seasonal = False,
+        special = False,
+        pic1 = '',
+    )
+    deja_brew = Product (
+        name = 'Deja Brew',
+        top_scent = 'Coffee, Hazelnut',
+        base_scent = 'Vanilla, Caramel, Sweet Cream',
+        lid_color = 'Gold',
+        description = 'Need the daily coffee fix but without the caffeine? This candle is perfect for you! It leaves your place smelling like freshly brewed coffee in the morning.  A perfect scent if you like your place smelling like a coffee shop with a shot of espresso, whipped Cream, and caramel syrup.',
+        price  = 20,
+        quantity = 5,
+        seasonal = False,
+        special = False,
+        pic1 = "",
+        pic2 = "",
+    )
+    hidden_page = Product (
+        name = 'Hidden Page',
+        top_scent = 'Cedarwood, Leather, Honey',
+        base_scent = 'Sandalwood, Tonka',
+        lid_color = 'Gold',
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
 
-#     )
+    example = Product (
+        name =
+        top_scent =
+        base_scent =
+        lid_color =
+        description =
+        price  = 
+        quantity =
+        seasonal =
+        special =
+        pic1 = 
+        pic2 = 
+    )
+    # example = Product (
+    #     name =
+    #     top_scent =
+    #     base_scent =
+    #     lid_color =
+    #     description =
+    #     price  = 
+    #     quantity =
+    #     seasonal =
+    #     special =
+    #     pic1 = 
+    #     pic2 = 
+    # )
