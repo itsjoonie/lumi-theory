bec86f820c5255cf32f6b8079f090046b670f945
june <luo.jnny@gmail.com>
2021-11-16 22:06:43 -0800

    unnested the data

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -2,13 +2,13 @@ from app.api.auth_routes import validation_errors_to_error_messages
 from flask import Blueprint, request
 from sqlalchemy.sql.functions import user
 from flask_login import login_required, current_user
-from app.models import User, db, Product, product
-
+from app.models import User, db, Product
 
 product_routes = Blueprint('products', __name__)
 
 @product_routes.route('/', methods=['GET'])
-def get_products():
+def all_products():
     products = Product.query.all()
-    print(products, "WHAT IS PRODUCTSSSSSSSSSSSSSS")
-    return {'products': [product.to_dict() for product in products]}
+    return {product.to_dict()['id']: product.to_dict() for product in products}
+    # products = Product.query.all()
+    # return {'products': [product.to_dict() for product in products]}
