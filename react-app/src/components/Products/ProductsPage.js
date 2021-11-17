b7f240f45f4b108f38d4f671e1974db753a49c59
june <luo.jnny@gmail.com>
2021-11-16 23:14:52 -0800

    populating products img

--- react-app/src/components/Products/ProductsPage.js
+++ react-app/src/components/Products/ProductsPage.js
@@ -7,7 +7,25 @@ function ProductsPage (){
     const products = Object.values(useSelector((state)=>(state.product)))
     console.log(products, "WTHAT IS THIS PRODUCTS")
     return (
-        <div className='productPages'>
+        <div className='productsPages'>
+            {/* banners  maybe*/}
+            {/* products page sidebar */}
+
+            <div className='products-item-container'>
+                <div className='products-item-square'>
+                    {products?.map(oneProduct =>(<div key={oneProduct.id} className='products-item-content'>
+                        <div className='products-item-card'>
+                            <img className='products-item-pic' src={oneProduct.pic1} alt='pic'/>
+
+                        </div>
+                    
+                    </div>
+                    ))}
+
+                </div>
+
+
+            </div>
          
         </div>
     )
