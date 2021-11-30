f6d794b7a7985fe60292bedad9fa067c8188f47d
june <luo.jnny@gmail.com>
2021-11-30 13:39:00 -0800

    product images toggle is working

--- react-app/src/components/Products/IndividualProductPage.js
+++ react-app/src/components/Products/IndividualProductPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import './IndividualProductPage.css'
@@ -7,27 +7,24 @@ function IndividualProductPage (){
     const params = useParams()
     const productId = params.id
     const product = useSelector((state)=>(state.product[productId]))
-    console.log(product, "WHAT IS PARAM")
 
-
-    // use params and then get products[id], where the id is params 
-
+    const [selectedPic, setSelectedPic] = useState(product.pic1)
 
     return (
         <div className='individual-product-page'>
         {/* <div>Maybe the navigation section here?</div> */}
 
         {/* picture section */}
-            <div className="individual-candle-pictures">
-                <div className="display-candle-pic">
-
+            <div className='individual-candle-pictures'>
+                <div className='display-candle-pic'>
+                    <img src={selectedPic} alt='big pic'/>
                 </div>
-                <div className="candle-pics-selection">
-                    <img className='candle-pic' src={product.pic1} alt='pic'/>
+                <div className='candle-pics-selection' >
+                    <img className='candle-pic' src={product.pic1} alt='pic' onClick={() => setSelectedPic(product.pic1)}/>
                     {!product.pic2 ?
                         <div></div>
                         :
-                        <img className='candle-pic' src={product.pic2} alt='pic'/>
+                        <img className='candle-pic' src={product.pic2} alt='pic'onClick={() => setSelectedPic(product.pic2)}/>
                     }
                
                     
