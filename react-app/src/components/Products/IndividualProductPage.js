198d31ea37776e64000830fa5aa06cceae04a81c
june <luo.jnny@gmail.com>
2021-12-01 10:54:18 -0800

    working on individual product details

--- react-app/src/components/Products/IndividualProductPage.js
+++ react-app/src/components/Products/IndividualProductPage.js
@@ -10,24 +10,77 @@ function IndividualProductPage (){
 
     const [selectedPic, setSelectedPic] = useState(product.pic1)
 
+    const togglePic = () => {
+
+        //toggle using the data in src of the img, so have to query selector all
+        //for loop
+
+   
+
+        // setSelectedPic(product.pic2);
+        // const previewPic2 = document.getElementById('selection-pic2')
+        // const previewPic1 = document.getElementById('selection-pic1')
+        // if(setSelectedPic === product.pic2){
+        //     previewPic2.classList.add('candle-pic-active');
+        // }else if(setSelectedPic === product.pic1){
+        //     previewPic1.classList.remove('candle-pic-active')
+        // }
+        
+    }
+
+        const seasonalSpecial = () =>{
+            if(!product.special && product.seasonal) {
+                return <span>Seasonal single wicked candle</span>
+            } else if(product.special && !product.seasonal) {
+                return <span>Special edition single wicked candle</span>
+            } else {
+                return <span>Original single wicked candle</span>
+            }
+        }
+
+    
+
     return (
         <div className='individual-product-page'>
         {/* <div>Maybe the navigation section here?</div> */}
 
         {/* picture section */}
-            <div className='individual-candle-pictures'>
-                <div className='display-candle-pic'>
-                    <img src={selectedPic} alt='big pic'/>
-                </div>
-                <div className='candle-pics-selection' >
-                    <img className='candle-pic' src={product.pic1} alt='pic' onClick={() => setSelectedPic(product.pic1)}/>
-                    {!product.pic2 ?
+            <div className='individual-product-container'>
+                <div className='individual-candle-pictures'>
+                    <div className='display-candle-pic'>
+                        <img src={selectedPic} alt='big pic'class/>
+                    </div>
+                    <div className='candle-pics-selections' >
+                        <img id='selection-pic1' src={product.pic1} alt='pic' onClick={() => setSelectedPic(product.pic1)}/>
+
+                        {!product.pic2 ?
                         <div></div>
                         :
-                        <img className='candle-pic' src={product.pic2} alt='pic'onClick={() => setSelectedPic(product.pic2)}/>
-                    }
-               
-                    
+                        <img id='selection-pic2' src={product.pic2} alt='pic'onClick={() => setSelectedPic(product.pic2)}/>
+                        } 
+                    </div>
+                    {/* might move seasonalSpecial function here */}
+                </div>
+                <div className='individual-product-details'>
+                    <div>
+                        <h1>{product.name}</h1>
+                        <h2>{seasonalSpecial()}</h2>
+                        <p>Star Rating Here and write a review</p>
+                        <h3><span>$</span>{product.price}.00</h3>
+                        <div></div>
+                    </div>
+                    <div>
+                        {/* quantity box and add to bag button  */}
+                    </div>
+                    <div>
+                        <p>{product.description}</p>
+                        <div>
+                        <h4>Frangrance Notes:</h4>
+                        <p></p>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
 
