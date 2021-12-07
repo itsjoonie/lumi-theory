83d9edb57d3f80e9e1988b82e997f9519172ac37
june <luo.jnny@gmail.com>
2021-12-07 12:03:43 -0800

    review store inprogress

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -1,8 +1,26 @@
 const LOAD_REVIEWS = 'review/LOAD_REVIEWS';
+const ADD_REVIEW = 'review/ADD_REVIEW';
+const UPDATE_REVIEW = 'review/UPDATE_REVIEW'
+const DELETE_REVIEW = 'review/DELETE_REVIEW'
 
 const loadReviews = (reviews) => ({
     type: LOAD_REVIEWS,
     reviews
+})
+
+const addReview = (review) => ({
+    type: ADD_REVIEW,
+    review
+})
+
+const updateReview = (review) => ({
+    type: UPDATE_REVIEW,
+    review
+})
+
+const deleteReview = (reviewId) => ({
+    type: DELETE_REVIEW,
+    reviewId
 })
 
 export const getAllReviews = () => async (dispatch) => {
@@ -12,11 +30,22 @@ export const getAllReviews = () => async (dispatch) => {
         const data = await res.json()
         dispatch(loadReviews(data))
     } else {
-        return ' review error'
+        return 'review error'
     }
 }
 
-// export const 
+export const createReview = (user_id, product_id, title, rating, body) => async (dispatch) => {
+    const res = await fetch(`/api/reviews/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id, product_id, title, rating, body
+        })
+
+    })
+}
 
 const initialState ={};
 
