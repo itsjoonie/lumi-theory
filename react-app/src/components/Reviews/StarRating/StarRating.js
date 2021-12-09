1da3ff53fb21bf5c3860a24b6835ccacbad4cec4
june <luo.jnny@gmail.com>
2021-12-08 17:10:59 -0800

    starRating component completed

--- react-app/src/components/Reviews/StarRating/StarRating.js
+++ react-app/src/components/Reviews/StarRating/StarRating.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './StarRating.css'
 
 function StarRating () {
+    const [rating, setRating] = useState(null)
+    const [hover, setHover] = useState(null)
+
     return (
         <>
             <div> 
-                {[...Array(5)].map(star => {
+                {[...Array(5)].map((star, i) => {
+                    const ratingValue = i + 1
+
                     return (
                     <label>
-                        <input type='radio' className='rating' />
-                            <i className='fas fa-star'></i>
+                        <input type='radio' className='rating' 
+                        value={ratingValue}
+                        onClick={() => setRating(ratingValue)}
+                        
+                        />
+                            <i className='fas fa-star' id='star' 
+                            style={{ color: ratingValue <= ( hover || rating ) ? "#ffd166" :"#ced4da"}}
+                            onMouseOver={()=> setHover(ratingValue)}
+                            onMouseLeave={()=> setHover(null)}></i>
                    
                     </label>
                     ) 
