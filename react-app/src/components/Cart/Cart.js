658a8b5393729f53af7682dcd98e8baaceb69039
june <luo.jnny@gmail.com>
2021-12-20 16:28:12 -0800

    cart

--- react-app/src/components/Cart/Cart.js
+++ react-app/src/components/Cart/Cart.js
@@ -24,16 +24,58 @@ function Sidebar() {
                 <Link to='#' className='menu-bars'>
                     <i class="fas fa-arrow-circle-right"></i>
                 </Link>
+                   <h1 className='cart-header'>My Cart (#)</h1>
                 </li>
-                  {/* CartItem */}
-                  {
-                    cart.map(item => 
-                    (
-                      <li>
-                        
-                      </li>
-                    ))
-                  }
+               
+                <div className='itemsInCart-container'>
+                    {/* CartItem */}
+                    
+                    {
+                      cart.map(item => 
+                      (
+                   
+                          <div className='items-detail-containers'>
+                            <div className='items-details'>
+                                <div>
+                                  <img className='cartItemImg' src={item.pic1} alt='pic'/>
+                                </div>
+                                <div className='item-detail-info'>
+                                  <h1 className='item-name'>{item.name}</h1>
+                                  <div className='item-price-info'>
+                                    <div>
+                                      <h2>Price:</h2>
+                                    </div>
+                                    <div>
+                                      <p><span>$</span>{item.price}.00</p>
+                                    </div>
+                                  </div>
+                                  <div className='item-price-info'>
+                                    <div>
+                                      <h2>Quantity:</h2>
+                                    </div>
+                                    <div>
+                                      <p>{item.quantity}</p>
+                                    </div>
+                                  </div>
+                                  
+                                </div>
+                            </div>
+                            <div className='item-total-price'>
+                                    <div>
+                                      <h3>Total:</h3>
+                                    </div>
+                                    <div>
+                                      <h3><span>$</span>{item.price*item.quantity}</h3>
+                                    </div>
+                                </div>
+                              <hr className='item-divider'/>
+                          </div>
+
+                          
+                      
+                      ))
+                    }
+                  </div>
                 
             </ul>
             </nav>
