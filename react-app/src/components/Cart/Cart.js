f6f0b7a0ad427915131d3525b295d09a3ca84fa6
june <luo.jnny@gmail.com>
2021-12-20 18:52:57 -0800

    checkout css

--- react-app/src/components/Cart/Cart.js
+++ react-app/src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux"
 import { SidebarData } from '../Modal/SidebarData.js';
 import './Cart.css';
+import CartTotal from './CartTotal.js';
 
 
 function Sidebar() {
@@ -30,56 +31,72 @@ function Sidebar() {
                 <div className='itemsInCart-container'>
                     {/* CartItem */}
                     
-                    {
-                      cart.map(item => 
-                      (
-                   
-                          <div className='items-detail-containers'>
-                            <div className='items-details'>
-                                <div>
-                                  <img className='cartItemImg' src={item.pic1} alt='pic'/>
-                                </div>
-                                <div className='item-detail-info'>
-                                  <h1 className='item-name'>{item.name}</h1>
-                                  <div className='item-price-info'>
-                                    <div>
-                                      <h2>Price:</h2>
-                                    </div>
-                                    <div>
-                                      <p><span>$</span>{item.price}.00</p>
-                                    </div>
+                    { cart.length ?
+                        cart.map(item => 
+                        (
+                    
+                            <div className='items-detail-containers'>
+                              <div className='items-details'>
+                                  <div>
+                                    <img className='cartItemImg' src={item.pic1} alt='pic'/>
                                   </div>
-                                  <div className='item-price-info'>
-                                    <div>
-                                      <h2>Quantity:</h2>
+                                  <div className='item-detail-info'>
+                                    <h1 className='item-name'>{item.name}</h1>
+                                    <div className='item-price-info'>
+                                      <div>
+                                        <h2>Price:</h2>
+                                      </div>
+                                      <div>
+                                        <p><span>$</span>{item.price}.00</p>
+                                      </div>
                                     </div>
-                                    <div>
-                                      <p>{item.quantity}</p>
+                                    <div className='item-price-info'>
+                                      <div>
+                                        <h2>Quantity:</h2>
+                                      </div>
+                                      <div className='item-add-subtract'>
+                                        <div>
+                                          <button className='add-subtract-btn'><i className="fas fa-minus-circle add-sub-btn-img"></i></button>
+                                        </div>
+                                        <div>
+                                          <p className='item-quantity-amt'>{item.quantity}</p>
+                                        </div>
+                                        <div>
+                                          <button className='add-subtract-btn'><i className="fas fa-plus-circle add-sub-btn-img"></i></button>
+                                        </div>
+                                      </div>
                                     </div>
+                                    
                                   </div>
-                                  
-                                </div>
+                              </div>
+                              <div className='item-total-price'>
+                                      <div>
+                                        <h3>Total:</h3>
+                                      </div>
+                                      <div>
+                                        <h3><span>$</span>{item.price*item.quantity}</h3>
+                                      </div>
+                                  </div>
+                                <hr className='item-divider'/>
                             </div>
-                            <div className='item-total-price'>
-                                    <div>
-                                      <h3>Total:</h3>
-                                    </div>
-                                    <div>
-                                      <h3><span>$</span>{item.price*item.quantity}</h3>
-                                    </div>
-                                </div>
-                              <hr className='item-divider'/>
-                          </div>
-
                           
-                      
                       ))
+                      :
+                      <div>
+                        <h1>No item</h1>
+                      </div>
                     }
+
+                    
                   </div>
-                
+                  <CartTotal/>
+                  
+                  
             </ul>
+                    
             </nav>
-     
+          
+        
     </div>
   );
 }
---
+++ react-app/src/components/Cart/CartTotal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './CartTotal.css'
+
+function CartTotal () {
+    return (
+        <div className='checkout-container'>
+            <h1>Total</h1>
+            <button><i class="fas fa-lock"></i>Checkout</button>
+        </div>
+    )
+}
+
+export default CartTotal
