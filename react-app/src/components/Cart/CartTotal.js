a2c778be9e41a35f2c84bcc8543a1b66d9e8f109
june <luo.jnny@gmail.com>
2021-12-20 20:04:52 -0800

    checkout

--- react-app/src/components/Cart/CartTotal.js
+++ react-app/src/components/Cart/CartTotal.js
@@ -4,7 +4,19 @@ import './CartTotal.css'
 function CartTotal () {
     return (
         <div className='checkout-container'>
-            <h1>Total</h1>
+            <div className='checkout-calculation-container'>
+                <div className='checkout-calculation-info'>
+
+                        <div className='tax'><p>Tax:</p></div>
+                        <div className='taxNum'> <p>$5</p></div>
+                        <div className='shipping'><p>Shipping:</p></div>
+                        <div className='shippingNum'>shipping amt</div>
+                        <div className='total'><h1>Total:</h1></div>
+                        <div className='totalNum'><h1>total amt</h1></div>
+      
+                </div>
+    
+            </div>
             <button><i class="fas fa-lock"></i>Checkout</button>
         </div>
     )
